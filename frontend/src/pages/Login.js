e5c7724264252e99180a2bd8877b63215b90a95b
Laura Grassi <laura@MacBook-Air-de-Laura.local>
2019-08-21 20:52:04 -0300

    Pagina de Login + Routes

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <form>
+      <img src={logo} alt="tindev"/>
+      <input type="text" placeholder="Digite seu usúario do Github"/>
+      <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <h1>Oi</h1>
+  );
+}
