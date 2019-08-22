bdc302fcb1564a99712b260d500560fcc9f61f91
Laura Grassi <laura_grassig@outlook.com>
2019-08-22 19:51:14 -0300

    Routes add

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -4,8 +4,6 @@ export default function Main() {
   return (
     <div>
       <h1>Oi</h1>
-      <h2>tudo bem com voce?</h2>
-      <h3>Só quero que o commit me note. TESTE PF</h3>
     </div>
   );
 }
